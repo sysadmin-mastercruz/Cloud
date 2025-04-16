0a2fa7c3cad1c17825b2dd1a809021ae823c3a11
Ricardo Cruz <ric_cruz22@hotmail.com>
2025-04-16 22:24:28 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 from app import create_app
+from flask_cors import CORS  # Importar suporte a CORS
 
+# Criar a app Flask
 app = create_app()
 
+# Ativar CORS para permitir chamadas de qualquer origem (útil para Swagger UI, por exemplo)
+CORS(app)
+
+# Função para listar todas as rotas disponíveis na API
 def listar_rotas():
     print("\n📌 Rotas disponíveis:")
     for rule in app.url_map.iter_rules():
         print(f"{rule.endpoint:30s} ➜ {rule}")
 
-
+# Iniciar a aplicação
 if __name__ == '__main__':
     listar_rotas()
     app.run(debug=True, host='0.0.0.0')
